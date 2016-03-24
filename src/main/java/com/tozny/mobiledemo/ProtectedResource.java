04b6e7919a54333e29e6101c5aa09144898d7a31
Jesse Hallett <jesse@galois.com>
2016-03-24 14:07:27 -0700

    Reject requests for protected resource if user is not authenticated.

--- src/main/java/com/tozny/mobiledemo/ProtectedResource.java
+++ src/main/java/com/tozny/mobiledemo/ProtectedResource.java
@@ -10,21 +10,39 @@
 
 package com.tozny.mobiledemo;
 
+import java.security.Principal;
 import java.util.Collections;
 import java.util.Map;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 
 @Path("/protected/secretmessage")
 public class ProtectedResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Map<String,String> getMessage() {
-        return Collections.<String,String>singletonMap("message", "You are authenticated.");
+    public Response getMessage(@Context SecurityContext securityContext) {
+        Principal user = securityContext != null ? securityContext.getUserPrincipal() : null;
+
+        if (user == null) {
+            return Response
+                .status(Response.Status.UNAUTHORIZED)
+                .header("WWW-Authenticate", "Bearer")
+                .build();
+        }
+
+        return Response.ok(
+                Collections.<String,String>singletonMap("message",
+                    "You are authenticated as " + user.getName()
+                    )
+                )
+            .build();
     }
 
 }
